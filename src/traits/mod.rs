de2ddfc134f9eae923a66a689482618b14cab9e0
Cass <cnegrich@gmail.com>
2024-12-08 11:10:06 -0700

    refactor: removes dead code

--- src/traits/mod.rs
+++ src/traits/mod.rs
@@ -1,6 +1,1 @@
-pub mod distance;
-pub mod generator;
-pub mod intersection;
-pub mod numbers;
-pub mod plot;
-pub mod segment;
+
--- src/types/mod.rs
+++ src/types/mod.rs
@@ -1,5 +1,2 @@
 pub mod circle;
-pub mod ordering;
 pub mod point;
-pub mod polygon;
-pub mod rectangle;
