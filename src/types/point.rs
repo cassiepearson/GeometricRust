7c58d73a75c3f14cceb60e62740a5a0979ba4dc9
Christopher Negrich <cnegrich@gmail.com>
2024-12-09 19:49:46 -0700

    test: adds unit tests to points struct

--- src/types/point.rs
+++ src/types/point.rs
@@ -17,3 +17,29 @@ where
         Self { x, y }
     }
 }
+
+#[cfg(test)]
+mod point_tests {
+    use super::*;
+
+    #[test]
+    fn test_new_usize() {
+        let out = Point::new(0_usize, 0_usize);
+        let expected = Point { x: 0_usize, y: 0_usize };
+        assert_eq!(out, expected);
+    }
+
+    #[test]
+    fn test_new_isize() {
+        let out = Point::new(0_isize, 0_isize);
+        let expected = Point { x: 0_isize, y: 0_isize };
+        assert_eq!(out, expected);
+    }
+
+    #[test]
+    fn test_new_float() {
+        let out = Point::new(0_f64, 0_f64);
+        let expected = Point { x: 0_f64, y: 0_f64 };
+        assert_eq!(out, expected);
+    }
+}
